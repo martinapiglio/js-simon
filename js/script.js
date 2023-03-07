8d31af2fb6e14297e54b45121b6dcc16d7df5cff
martinapiglio <martina.piglio@gmail.com>
2023-03-07 13:19:19 +0100

    function random numbers array+check click button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log('test');
+const playButton = document.getElementById('play-button');
+
+playButton.addEventListener('click', function() {
+    let x = randomArray(5, 1, 100);
+    console.log(x);
+});
+
+
+
+
+//FUNCTION: create an array of random numbers---------------------------
+function randomArray(arrayLenght, min, max) {
+
+    const numbersArray = []; //empty array
+    
+    while (numbersArray.length < arrayLenght) {
+    
+        let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        // check to avoid duplicate numbers
+        if (!numbersArray.includes(randomNumber)) {
+    
+            numbersArray.push(randomNumber);
+    
+        };
+    
+    };
+    
+    return numbersArray;
+    
+};
